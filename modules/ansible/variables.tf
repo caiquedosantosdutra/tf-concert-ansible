ef7e8cb57f597bb99af47fe9672de6cafbe6c6e8
Caique Dutra Santos <caique.santos@ibm.com>
2026-06-02 20:57:45 -0300

    fixing issues ec2 and ansible v6

--- modules/ansible/variables.tf
+++ modules/ansible/variables.tf
@@ -15,3 +15,37 @@ variable "aap_job_template_id" {
   type        = number
   description = "ID do job template no AAP"
 }
+
+# modules/ansible/variables.tf
+
+variable "instance_public_ip" {
+  type        = string
+  description = "IP público da EC2 Windows"
+}
+
+variable "windows_password" {
+  type        = string
+  sensitive   = true
+  description = "Senha do Administrator da EC2 Windows"
+}
+
+variable "aap_job_template_id" {
+  type        = number
+  description = "ID do job template no AAP"
+}
+
+variable "aap_host" {
+  type        = string
+  description = "URL do AAP"
+}
+
+variable "aap_username" {
+  type        = string
+  description = "Usuário do AAP"
+}
+
+variable "aap_password" {
+  type        = string
+  sensitive   = true
+  description = "Senha do AAP"
+}
