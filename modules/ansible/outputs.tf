c50b847daefe05d4b83057a69ee670a5f019cdbc
Caique Dutra Santos <caique.santos@ibm.com>
2026-06-02 16:42:36 -0300

    fixing issues ec2 and ansible v4

--- modules/ansible/outputs.tf
+++ modules/ansible/outputs.tf
@@ -1,11 +1,11 @@
 # modules/ansible/outputs.tf
 
-output "job_id" {
-  value       = aap_job.this.id
-  description = "ID do job no AAP"
-}
-
 output "job_status" {
   value       = aap_job.this.status
   description = "Status final do job"
+}
+
+output "job_url" {
+  value       = aap_job.this.url
+  description = "URL do job no AAP"
 }
