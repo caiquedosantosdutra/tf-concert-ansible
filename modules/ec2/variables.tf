56f135d3b14d06facd47ba7eb6a2650557e08962
Caique Dutra Santos <caique.santos@ibm.com>
2026-06-02 16:03:21 -0300

    fixing issues ec2 and ansible v4

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -8,7 +8,7 @@ variable "instance_type" {
 variable "ami" {
   type        = string
   description = "AMI Windows Server 2022"
-  default     = "ami-0f9c44e98edf38a2b"  # Windows Server 2022 us-east-1
+  default     = "ami-0909cee4864578472"  # Windows Server 2022 us-east-1
 }
 
 variable "vpc_id" {
