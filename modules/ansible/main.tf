9ce97f12e39138d80015603535107f6fb500e7e7
Caique Dutra Santos <caique.santos@ibm.com>
2026-06-02 21:03:01 -0300

    fixing issues ec2 and ansible v6

--- modules/ansible/main.tf
+++ modules/ansible/main.tf
@@ -1,4 +1,5 @@
 # modules/ansible/main.tf
+
 terraform {
   required_providers {
     aap = {
@@ -12,6 +13,14 @@ terraform {
   }
 }
 
+provider "restapi" {
+  uri                  = var.aap_host
+  username             = var.aap_username
+  password             = var.aap_password
+  insecure             = true
+  write_returns_object = true
+}
+
 resource "aap_inventory" "this" {
   name         = "servidores-tf"
   organization = 1
@@ -22,47 +31,38 @@ resource "aap_host" "this" {
   inventory_id = aap_inventory.this.id
 
   variables = jsonencode({
-    ansible_connection                = "winrm"
-    ansible_host                      = var.instance_public_ip
-    ansible_user                      = "Administrator"
-    ansible_winrm_transport           = "basic"
-    ansible_winrm_port                = 5985
+    ansible_connection                   = "winrm"
+    ansible_host                         = var.instance_public_ip
+    ansible_user                         = "Administrator"
+    ansible_password                     = var.windows_password
+    ansible_winrm_transport              = "basic"
+    ansible_winrm_port                   = 5985
     ansible_winrm_server_cert_validation = "ignore"
+    ansible_shell_type                   = "powershell"
+    ansible_become                       = false
   })
 }
-resource "aap_job_template" "this" {
-  name            = "configurar-windows"
-  organization_id = 1
-  inventory_id    = aap_inventory.this.id
-  project_id      = var.aap_project_id
-  playbook        = var.playbook
+
+# Atualiza o job template com o inventário via API
+resource "restapi_object" "update_job_template" {
+  path = "/api/controller/v2/job_templates/${var.aap_job_template_id}/"
+  data = jsonencode({
+    inventory = tonumber(aap_inventory.this.id)
+  })
+
+  depends_on = [aap_inventory.this]
 }
+
 resource "aap_job" "this" {
   job_template_id = var.aap_job_template_id
   inventory_id    = aap_inventory.this.id
-
 
   extra_vars = jsonencode({
     target_host = var.instance_public_ip
   })
 
-  depends_on = [aap_host.this]
-}
-provider "restapi" {
-  uri                  = var.aap_host
-  username             = var.aap_username
-  password             = var.aap_password
-  insecure             = true
-  write_returns_object = true
-}
-
-# Atualiza o job template com o inventário criado
-resource "restapi_object" "update_job_template" {
-  path         = "/api/controller/v2/job_templates/${var.aap_job_template_id}/"
-  method       = "PATCH"
-  data         = jsonencode({
-    inventory = aap_inventory.this.id
-  })
-
-  depends_on = [aap_inventory.this]
+  depends_on = [
+    aap_host.this,
+    restapi_object.update_job_template
+  ]
 }
